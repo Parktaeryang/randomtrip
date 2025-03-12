072a133c2e70bfcca222900fcd07bf5ff8b239b5
Parktaeryang <skygve2@gmail.com>
2025-03-12 19:07:10 +0900

    no message

---
+++ src/test/java/com/travel/randomtrip/RandomtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.randomtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomtripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
