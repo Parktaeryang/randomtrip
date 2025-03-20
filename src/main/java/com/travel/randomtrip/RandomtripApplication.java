670eb32d719beb1ad82dc40cd975babce0ddf8c6
Ryang <62952298+Parktaeryang@users.noreply.github.com>
2025-03-20 18:52:05 +0900

    Update RandomtripApplication.java
    io.github.cdimascio.dotenv.Dotenv 삭제

--- src/main/java/com/travel/randomtrip/RandomtripApplication.java
+++ src/main/java/com/travel/randomtrip/RandomtripApplication.java
@@ -9,12 +9,6 @@ import java.util.Objects;
 @SpringBootApplication
 public class RandomtripApplication {
 
-    static {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("TOURISM_SERVICE_KEY", Objects.requireNonNull(dotenv.get("TOURISM_SERVICE_KEY")));
-        System.setProperty("KAKAO_REST_KEY", Objects.requireNonNull(dotenv.get("KAKAO_REST_KEY")));
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(RandomtripApplication.class, args);
     }
